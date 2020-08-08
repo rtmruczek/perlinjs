b3b3ec75f870b888afcf72b41d4f73b1e1a5aaeb
Robert Mruczek <rtmruczek@gmail.com>
2020-08-08 18:35:03 -0400

    got some noise showin

--- index.ts
+++ index.ts
@@ -1,17 +1,86 @@
-const gridLength = 8;
-const grid: Vector[][] = [];
+import {
+  Vector,
+  lerp,
+  dotProduct,
+  fade,
+  normalize,
+  normalizeNum,
+} from './utils';
 
-for (let i = 0; i < gridLength; i++) {
-  const vectorVals = [...new Array(gridLength)].map(() => ({
+const boxSize = 50;
+const noiseLength = 500;
+
+if (noiseLength % boxSize !== 0) {
+  throw new Error('noiseLength must be divisible by boxSize');
+}
+const boxes: Vector[][] = [];
+const noise: number[][] = [...new Array(noiseLength)].map(() =>
+  [...new Array(noiseLength)].map(() => 0)
+);
+
+const boxesPerRow = noiseLength / boxSize;
+
+for (let i = 0; i <= boxesPerRow; i++) {
+  const vectorVals = [...new Array(boxesPerRow + 1)].map(() => ({
     x: Math.random() * 2 - 1,
     y: Math.random() * 2 - 1,
   }));
-  grid.push(vectorVals);
+  boxes.push(vectorVals);
 }
-console.log(grid);
+console.log(boxes);
 
-// for (let i = 0; i < grad.length; i++) {
-//   for (let j = 0; j < grad[0].length; j++) {
-//     perlinValues[i][j] = perlin(i, j);
-//   }
-// }
+let ctx: CanvasRenderingContext2D;
+
+window.onload = function () {
+  ctx = document.getElementsByTagName('canvas')[0].getContext('2d');
+};
+
+setTimeout(() => {
+  for (let y = 0; y < noise[0].length; y++) {
+    // gimme the noise
+    for (let x = 0; x < noise.length; x++) {
+      // determine which box i'm in
+      const boxX = Math.floor(x / boxSize);
+      const boxY = Math.floor(y / boxSize);
+
+      const xMin = boxX * boxSize;
+      const yMin = boxY * boxSize;
+
+      const xMax = xMin + boxSize - 1;
+      const yMax = yMin + boxSize - 1;
+
+      const sx = x - xMin;
+      const sy = y - yMin;
+
+      const gradientVectorTopLeft = boxes[boxX][boxY];
+      const gradientVectorTopRight = boxes[boxX + 1][boxY];
+      const gradientVectorBottomLeft = boxes[boxX][boxY + 1];
+      const gradientVectorBottomRight = boxes[boxX + 1][boxY + 1];
+
+      const vectorTopLeft = normalize({ x: x - xMin, y: y - yMin });
+      const vectorTopRight = normalize({ x: x - xMax, y: y - yMin });
+      const vectorBottomLeft = normalize({ x: x - xMin, y: y - yMax });
+      const vectorBottomRight = normalize({ x: x - xMax, y: y - yMax });
+
+      const dotTL = dotProduct(gradientVectorTopLeft, vectorTopLeft);
+      const dotTR = dotProduct(gradientVectorTopRight, vectorTopRight);
+      const dotBL = dotProduct(gradientVectorBottomLeft, vectorBottomLeft);
+      const dotBR = dotProduct(gradientVectorBottomRight, vectorBottomRight);
+
+      let result = lerp(
+        lerp(dotBL, dotTL, sx / boxSize),
+        lerp(dotBR, dotTR, sx / boxSize),
+        sy / boxSize
+      );
+      noise[x][y] = result;
+
+      const scaledAlpha = normalizeNum(result);
+      const rgbstr = `rgba(0,0,0,${scaledAlpha})`;
+
+      ctx.fillStyle = rgbstr;
+      ctx.fillRect(x, y, 1, 1);
+    }
+  }
+  console.log('done');
+  console.log(noise);
+}, 500);
--- utils.ts
+++ utils.ts
@@ -1,53 +1,31 @@
 /* Function to linearly interpolate between a0 and a1
  * Weight w should be in the range [0.0, 1.0]
  */
-const lerp = (a: number, b: number, weight: number) => {
+export const lerp = (a: number, b: number, weight: number) => {
   return (1.0 - weight) * a + weight * b;
 };
 
-const dotGridGradient = (
-  grad: number[][][],
-  ix: number,
-  iy: number,
-  x: number,
-  y: number
-) => {
-  const dx = x - ix;
-  const dy = y - iy;
-
-  return dx * grad[iy][ix][0] + dy * grad[iy][ix][1];
-};
-
-const dotProduct = (a: Vector, b: Vector) => {
+export const dotProduct = (a: Vector, b: Vector) => {
   return a.x * b.x + a.y * b.y;
 };
 
-const perlin = (x: number, y: number) => {
-  const x1 = x + 1;
-  const y1 = y + 1;
+//Optimized version (less multiplications)
+export function fade(t: number) {
+  return 6 * Math.pow(t, 5) - 15 * Math.pow(t, 4) + 10 * Math.pow(t, 3);
+}
 
-  const sx = x1 - x;
-  const sy = y1 - y;
+export function normalize(v: Vector) {
+  const length = Math.sqrt(Math.pow(v.x, 2) + Math.pow(v.y, 2));
+  return {
+    x: v.x / length,
+    y: v.y / length,
+  };
+}
 
-  const topRight: Vector = {
-    x: sx - 1.0,
-    y: sy - 1.0,
-  };
-  const topLeft: Vector = {
-    x: sx,
-    y: sy - 1.0,
-  };
-  const bottomRight: Vector = {
-    x: sx - 1.0,
-    y: sy,
-  };
-  const bottomLeft: Vector = {
-    x: sx,
-    y: sy,
-  };
-};
-
-type Vector = {
+export function normalizeNum(n: number) {
+  return n + 1 / 2;
+}
+export type Vector = {
   x: number;
   y: number;
 };
